afc820a792e6b5a57816f21dd4ef28064afc256e
oshiimizunohuta <g.ebodai@gmail.com>
2014-09-13 20:23:46 +0900

    ツイート内容変更

--- source/LitroKeyboard.js
+++ source/LitroKeyboard.js
@@ -4728,8 +4728,9 @@ LitroKeyboard.prototype = {
 			url += [
 				'url=' + encodeURIComponent('http://' + location.host + location.pathname 
 				+ (file.sound_id == 0 ? '' : '?sound_id=' + file.sound_id)),
-				'text=' + encodeURIComponent('"'+ file.title + '" play on the litrokeyboard!!'),
+				'text=' + encodeURIComponent(file.sound_id == 0 ? 'ブラウザでPSG音源DTM！' : ('"'+ file.title + '" play on the litrokeyboard!!')),
 				'hashtags=' + encodeURIComponent('litrokeyboard,dtm'),
+				'via=' + 'LitroKeyboard'
 				].join('&');
 			}
 		window.open(url, null,"width=640,height=480,scrollbars=yes");
